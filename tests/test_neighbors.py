a9220e14606f73c58c9bbc5d98de7c0030ded545
ghuynh <gael.huynh@gmail.com>
2024-02-12 11:00:36 +0100

    Sort imports

--- tests/test_neighbors.py
+++ tests/test_neighbors.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pymatgen.core import Structure
+
 from src.utils.neighbors import find_knn_in_shell
 
 NACL_POSCAR = """
@@ -15,15 +16,22 @@ direct
 """
 FIRST_SHELL_DIST = 2.794063078217634
 
+
 def test_find_knn_in_shell():
     structure = Structure.from_str(NACL_POSCAR, fmt="poscar")
-    
+
     # Test case 1: Basic test with a simple structure
     rcut = 3.0
     n_neighbors = 4
     expected_output = [
-        [FIRST_SHELL_DIST,] * 4,
-        [FIRST_SHELL_DIST,] * 4,
+        [
+            FIRST_SHELL_DIST,
+        ]
+        * 4,
+        [
+            FIRST_SHELL_DIST,
+        ]
+        * 4,
     ]
     output = find_knn_in_shell(structure, rcut, n_neighbors)
     nn_distance = [[s.nn_distance for s in sites] for sites in output]
@@ -33,8 +41,14 @@ def test_find_knn_in_shell():
     rcut = 4.5
     n_neighbors = 3
     expected_output = [
-        [FIRST_SHELL_DIST,] * 3,
-        [FIRST_SHELL_DIST,] * 3,
+        [
+            FIRST_SHELL_DIST,
+        ]
+        * 3,
+        [
+            FIRST_SHELL_DIST,
+        ]
+        * 3,
     ]
     output = find_knn_in_shell(structure, rcut, n_neighbors)
     nn_distance = [[s.nn_distance for s in sites] for sites in output]
@@ -44,8 +58,14 @@ def test_find_knn_in_shell():
     rcut = 2.0
     n_neighbors = 4
     expected_output = [
-        [FIRST_SHELL_DIST,] * 4,
-        [FIRST_SHELL_DIST,] * 4,
+        [
+            FIRST_SHELL_DIST,
+        ]
+        * 4,
+        [
+            FIRST_SHELL_DIST,
+        ]
+        * 4,
     ]
     output = find_knn_in_shell(structure, rcut, n_neighbors)
     nn_distance = [[s.nn_distance for s in sites] for sites in output]
