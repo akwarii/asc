fd490a535fbe96ace2d00c69f12e8fd0bda1f7fd
ghuynh <gael.huynh@gmail.com>
2024-02-15 18:00:18 +0100

    Formatting

--- src/api/gnome.py
+++ src/api/gnome.py
@@ -11,13 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Download GNoME data to a chosen directory."""
 
-from collections.abc import Sequence
 import os
-from absl import app
-from absl import flags
+from collections.abc import Sequence
+
+from absl import app, flags
 
 _DATA_DIR = flags.DEFINE_string(
     name="data_dir",
@@ -39,32 +38,30 @@ FILES = (
 
 
 def download_from_link(link: str, output_dir: str):
-  """Download a file from a public link using wget."""
-  os.system(f"wget {link} -P {output_dir}")
+    """Download a file from a public link using wget."""
+    os.system(f"wget {link} -P {output_dir}")
 
 
 def main(argv: Sequence[str]) -> None:
-  if len(argv) > 1:
-    raise app.UsageError("Too many command-line arguments.")
+    if len(argv) > 1:
+        raise app.UsageError("Too many command-line arguments.")
 
-  # Create output folder
-  output_folder = os.path.join(_DATA_DIR.value, FOLDER_NAME)
-  os.makedirs(output_folder, exist_ok=True)
+    # Create output folder
+    output_folder = os.path.join(_DATA_DIR.value, FOLDER_NAME)
+    os.makedirs(output_folder, exist_ok=True)
 
-  parent_directory = os.path.join(PUBLIC_LINK, BUCKET_NAME, FOLDER_NAME)
+    parent_directory = os.path.join(PUBLIC_LINK, BUCKET_NAME, FOLDER_NAME)
 
-  # Download LICENSE file
-  download_from_link(
-      os.path.join(PUBLIC_LINK, BUCKET_NAME, "LICENSE"), _DATA_DIR.value
-  )
+    # Download LICENSE file
+    download_from_link(os.path.join(PUBLIC_LINK, BUCKET_NAME, "LICENSE"), _DATA_DIR.value)
 
-  # Download data files.
-  for filename in FILES:
-    public_link = os.path.join(parent_directory, filename)
-    download_from_link(public_link, os.path.join(_DATA_DIR.value, FOLDER_NAME))
+    # Download data files.
+    for filename in FILES:
+        public_link = os.path.join(parent_directory, filename)
+        download_from_link(public_link, os.path.join(_DATA_DIR.value, FOLDER_NAME))
 
-  print(f"Done downloading data to directory: {_DATA_DIR.value}")
+    print(f"Done downloading data to directory: {_DATA_DIR.value}")
 
 
 if __name__ == "__main__":
-  app.run(main)
+    app.run(main)
