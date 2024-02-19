ee43b5de399fc840c9bbdba1c490e48ecb2f8600
ghuynh <gael.huynh@gmail.com>
2024-02-19 17:07:31 +0100

    Add planned augmentation and transformations

---
+++ src/data/augmentation/augmentation.py
@@ -0,0 +1,9 @@
+import torch
+
+# TODO implement the gaussian noise augmentation
+class RandomDisplacement(torch.nn.Module):
+    ...
+    
+# TODO implement the random expansion augmentation
+class RandomExpansion(torch.nn.Module):
+    ...
