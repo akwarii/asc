02b866faac7cc17d50dc32912c62aa2f5b8d650b
ghuynh <gael.huynh@gmail.com>
2024-03-14 16:44:15 +0100

    Solve issue in tests

---
+++ src/data/datasets/__init__.py
@@ -0,0 +1,5 @@
+from .aflow import Aflow
+from .csg import CSG
+from .gnome import Gnome
+from .in_memory import InMemoryDataset
+from .material_project import MaterialProject
--- tests/test_gnome.py
+++ tests/test_gnome.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 from unittest.mock import patch
 
-from src.data.datasets.gnome import Gnome
+from src.data.datasets import Gnome
 
 
 @patch("src.data.datasets.gnome.Gnome.check_exists", return_value=True)
-@patch("src.data.datasets.gnome.Gnome._load_data", return_value=(["data"], ["targets"]))
+@patch("src.data.datasets.gnome.Gnome.load", return_value=(["data"], ["targets"]))
 @patch("src.data.datasets.gnome.Gnome.download")
 def test_gnome_init(mock_exists, mock_data, mock_download):
     gnome = Gnome(root="data", download=False)
@@ -18,7 +18,7 @@ def test_gnome_init(mock_exists, mock_data, mock_download):
 
 
 @patch("src.data.datasets.gnome.Gnome.check_exists", return_value=True)
-@patch("src.data.datasets.gnome.Gnome._load_data", return_value=(["1", "2", "3"], [1, 2, 3]))
+@patch("src.data.datasets.gnome.Gnome.load", return_value=(["1", "2", "3"], [1, 2, 3]))
 def test_gnome_len(mock_exists, mock_data):
     gnome = Gnome(root="data", download=False)
     assert len(gnome) == 3
