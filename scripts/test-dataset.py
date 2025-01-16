b01408317a81bea025da7240e91628e4f27e9b5c
ghuynh <gael.huynh@gmail.com>
2025-01-16 12:19:53 +0100

    Add class count print statements to dataset summaries

--- scripts/test-dataset.py
+++ scripts/test-dataset.py
@@ -5,13 +5,17 @@ rcut = 6.0
 reload = True
 
 custom = CustomDataset(force_reload=reload, k=k, rcut=rcut)
+print("Num classes: ", custom.num_classes)
 custom.print_summary()
 
-csg = Aflow(force_reload=reload, k=k, rcut=rcut)
-csg.print_summary()
+aflow = Aflow(force_reload=reload, k=k, rcut=rcut)
+print("Num classes: ", aflow.num_classes)
+aflow.print_summary()
 
 csg = CSG(force_reload=reload, k=k, rcut=rcut)
+print("Num classes: ", csg.num_classes)
 csg.print_summary()
 
 mp = MaterialProject(force_reload=reload, k=k, rcut=rcut)
+print("Num classes: ", mp.num_classes)
 mp.print_summary()
