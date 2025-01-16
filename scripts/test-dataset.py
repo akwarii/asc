43d648853ac1b011d2348c6fbb08ea18c3958ada
ghuynh <gael.huynh@gmail.com>
2025-01-16 12:15:22 +0100

    Simplify test-dataset

--- scripts/test-dataset.py
+++ scripts/test-dataset.py
@@ -1,15 +1,17 @@
 from src.datasets import CSG, Aflow, CustomDataset, MaterialProject
 
-reload = False
+k = 12
+rcut = 6.0
+reload = True
 
-custom = CustomDataset(force_reload=reload, k=8, rcut=5.0)
-print(custom.get_summary())
+custom = CustomDataset(force_reload=reload, k=k, rcut=rcut)
+custom.print_summary()
 
-csg = Aflow(force_reload=reload, k=8, rcut=5.0)
-print(csg.get_summary())
+csg = Aflow(force_reload=reload, k=k, rcut=rcut)
+csg.print_summary()
 
-csg = CSG(force_reload=reload, k=8, rcut=5.0)
-print(csg.get_summary())
+csg = CSG(force_reload=reload, k=k, rcut=rcut)
+csg.print_summary()
 
-mp = MaterialProject(force_reload=reload, k=8, rcut=5.0)
-print(mp.get_summary())
+mp = MaterialProject(force_reload=reload, k=k, rcut=rcut)
+mp.print_summary()
