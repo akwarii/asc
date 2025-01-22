4b8f93ecb6da95e4ca5a3c7b142a65bd8d2a9662
ghuynh <gael.huynh@gmail.com>
2025-01-22 11:43:12 +0100

    updated model variable

--- scripts/test-training.py
+++ scripts/test-training.py
@@ -2,15 +2,11 @@ import torch
 import torch_geometric.transforms as T
 import torchmetrics
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import (
-    LearningRateFinder,
-    StochasticWeightAveraging,
-)
+from pytorch_lightning import callbacks as pl_callbacks
 from pytorch_lightning.loggers import CSVLogger
 from src.constants import DEFAULT_SEED
 from src.datamodule import CEGANNLightningDataset
 from src.datasets import CSG
-from src.models import CEGANN
 from src.module import CEGANNModule
 from src.transforms import LineGraph, RandomPerturbation
 from src.utils.dataset import random_split
@@ -18,7 +14,6 @@ from torch_geometric.loader import ImbalancedSampler
 
 seed_everything(DEFAULT_SEED)
 
-# Data management
 dataset = CSG(
     pre_transform=LineGraph(),
     transform=T.Compose(
@@ -44,10 +39,9 @@ datamodule = CEGANNLightningDataset(
     sampler=ImbalancedSampler(torch.tensor([data.y[0].item() for data in train_dataset])),
 )
 
-module = CEGANNModule(
+model = CEGANNModule(
     model_name="cegann",
     model_kwargs={"n_classes": dataset.num_classes},
-    compile=True,
     optimizer=torch.optim.AdamW,
     scheduler=torch.optim.lr_scheduler.StepLR,
     scheduler_params={"gamma": 0.5, "step_size": 100},
@@ -60,18 +54,19 @@ module = CEGANNModule(
         }
     ),
 )
+model = torch.compile(model)
 
 trainer = Trainer(
-    # fast_dev_run=True,
     max_epochs=100,
     precision="16-mixed",
     callbacks=[
-        # BatchSizeFinder(steps_per_trial=100),
-        LearningRateFinder(min_lr=1e-5, max_lr=0.1, num_training_steps=5_000),
-        StochasticWeightAveraging(swa_lrs=0.01),
+        pl_callbacks.BatchSizeFinder(steps_per_trial=100),
+        pl_callbacks.LearningRateFinder(min_lr=1e-5, max_lr=0.1, num_training_steps=5_000),
+        pl_callbacks.StochasticWeightAveraging(swa_lrs=0.01),
+        pl_callbacks.ModelCheckpoint(),
     ],
     logger=CSVLogger(save_dir="."),
     deterministic=True,
 )
 
-trainer.fit(model=module, datamodule=datamodule)
+trainer.fit(model=model, datamodule=datamodule)  # type: ignore
