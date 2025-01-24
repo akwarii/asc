e45b8ae281e3524da71b892c6ce870e740901666
ghuynh <gael.huynh@gmail.com>
2025-01-24 12:08:29 +0100

    Update scripts

---
+++ scripts/hparams.py
@@ -0,0 +1,184 @@
+import pickle
+from pathlib import Path
+from typing import Any
+
+import lightning as L
+import optuna
+import torch_geometric.transforms as T
+import torchmetrics
+from lightning.pytorch.callbacks import BatchSizeFinder, LearningRateFinder
+from optuna import distributions
+from optuna.importance import get_param_importances
+from optuna.trial import TrialState
+from optuna_integration import PyTorchLightningPruningCallback
+from src.constants import DEFAULT_SEED
+from src.datamodule import LightningDataset
+from src.module import Module
+from src.transforms import LineGraph, RandomPerturbation
+
+EPOCHS = 50
+K_NEIGH = 8
+BUDGET = 100
+STUDY_NAME = f"cegann-k{K_NEIGH}"
+STORAGE = f"sqlite:///{STUDY_NAME}.db"
+
+
+def get_sampler_and_pruner(
+    sampler_pkl_path: Path,
+) -> tuple[optuna.samplers.BaseSampler, optuna.pruners.BasePruner]:
+    if sampler_pkl_path.exists():
+        with open(sampler_pkl_path, "rb") as f:
+            sampler = pickle.load(f)
+    else:
+        sampler = optuna.samplers.TPESampler()
+    pruner = optuna.pruners.HyperbandPruner()
+
+    return sampler, pruner
+
+
+def report_trial_params(trial: optuna.trial.Trial) -> None:
+    print(f"Trial {trial.number}/{BUDGET}:")
+    print("    Parameters: ")
+    for param, value in trial.params.items():
+        print(f"  {param}: {value}")
+
+
+def report_statistics(study: optuna.study.Study) -> None:
+    prunned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
+    complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
+    failed_trials = study.get_trials(deepcopy=False, states=[TrialState.FAIL])
+    best_trial = study.best_trial
+
+    print("Study statistics: ")
+    print(f"  Number of trials: {len(study.trials)}")
+    print(f"  Number of complete trials: {len(complete_trials)}")
+    print(f"  Number of pruned trials: {len(prunned_trials)}")
+    print(f"  Number of failed trials: {len(failed_trials)}")
+
+    print("Best trial:")
+    print(f"  Value: {best_trial.value}")
+    print("  Params: ")
+    hparams_importance = get_param_importances(study)
+    for (param, value), (_, importance) in zip(
+        best_trial.params.items(), hparams_importance.items()
+    ):
+        print(f"    {param}: {value} ({importance:.2f})")
+
+
+def trial_step(hparams: dict[str, Any]) -> None:
+    trial = study.ask(fixed_distributions=hparams)
+
+    # Check whether we already evaluated the sampled hyperparameters
+    # If it exists, then use the existing value as trial duplicated the parameters.
+    for t in reversed(trial.study.get_trials(deepcopy=False)):
+        if trial.params == t.params:
+            study.tell(trial, t.value)
+
+    # Configure the Lightning Trainer
+    trainer = L.Trainer(
+        logger=True,
+        enable_checkpointing=False,
+        max_epochs=EPOCHS,
+        callbacks=[
+            BatchSizeFinder(steps_per_trial=10),
+            LearningRateFinder(min_lr=1e-5, max_lr=0.1),
+            PyTorchLightningPruningCallback(trial, monitor="val/acc"),
+        ],
+    )
+
+    # Configure the Lightning Module
+    model = Module(
+        model_name="cegann",
+        num_classes=datamodule.num_classes,
+        compile=True,
+        metrics=torchmetrics.MetricCollection(
+            {
+                "acc": torchmetrics.Accuracy(
+                    task="multiclass",
+                    num_classes=datamodule.num_classes,
+                ),
+            }
+        ),
+        warmup=100,
+        max_iters=trainer.max_epochs * len(datamodule.train_dataloader()),  # type: ignore
+        model_kwargs=trial.params,
+    )
+
+    # Log the hyperparameters and start the training
+    report_trial_params(trial)
+    if trainer.logger is not None:
+        trainer.logger.log_hyperparams(trial.params)
+
+    # Run the training loop and check if the trial was pruned
+    state = optuna.trial.TrialState.COMPLETE
+    try:
+        trainer.fit(model, datamodule=datamodule)
+    except optuna.TrialPruned:
+        state = optuna.trial.TrialState.PRUNED
+    except (Exception, KeyboardInterrupt):
+        state = optuna.trial.TrialState.FAIL
+
+    # If validation accuracy is not available, use None to indicate that the trial has failed.
+    val_acc = trainer.callback_metrics.get("val/acc", None)
+    if val_acc is not None:
+        val_acc = val_acc.item()
+
+    study.tell(trial, val_acc, state)
+
+
+if __name__ == "__main__":
+    L.seed_everything(DEFAULT_SEED)
+
+    sampler_pkl_path = Path(f"{STUDY_NAME}_sampler.pkl")
+    sampler, pruner = get_sampler_and_pruner(sampler_pkl_path)
+
+    study = optuna.create_study(
+        direction="maximize",
+        sampler=sampler,
+        pruner=pruner,
+        study_name=STUDY_NAME,
+        storage=STORAGE,
+        load_if_exists=True,
+    )
+
+    hparam_distributions = {
+        "edge_expansion_units": distributions.CategoricalDistribution([32, 64, 128, 256, 512]),
+        "angle_expansion_units": distributions.CategoricalDistribution([32, 64, 128, 256, 512]),
+        "num_radial": distributions.IntDistribution(20, 100),
+        "n_bond_conv": distributions.IntDistribution(1, 6),
+        "dropout": distributions.FloatDistribution(0.0, 0.8, step=0.1),
+        "classification_units": distributions.CategoricalDistribution([32, 64, 128, 256, 512]),
+        "classification_layers": distributions.IntDistribution(1, 4),
+    }
+
+    # Configure the Lightning Module. Only reload the dataset if there are no trials.
+    # It means that the number of neighbors may have changed.
+    if len(study.get_trials(deepcopy=False)) == 0:
+        force_reload = True
+    else:
+        force_reload = False
+
+    datamodule = LightningDataset(
+        dataset_name="csg",
+        lengths=(0.8, 0.2),
+        use_imbalance_sampler=True,
+        pre_transforms=LineGraph(),
+        transforms=[
+            T.NormalizeFeatures(["x", "edge_attr"]),
+            RandomPerturbation(std=0.05),
+        ],
+        num_workers=5,
+        k=K_NEIGH,
+        rcut=6.0,
+        force_reload=force_reload,
+    )
+
+    # Hyperparameter optimization loop
+    for _ in range(BUDGET):
+        trial_step(hparam_distributions)
+
+        with open(sampler_pkl_path, "wb") as f:
+            pickle.dump(study.sampler, f)
+
+    # Report the statistics of the study
+    report_statistics(study)
--- scripts/test-hparams.py
+++ scripts/test-hparams.py
@@ -1,50 +1,87 @@
+import pickle
+from pathlib import Path
+
+import lightning as L
 import optuna
-import pytorch_lightning as pl
 import torch_geometric.transforms as T
 import torchmetrics
+from lightning.pytorch.callbacks import BatchSizeFinder, LearningRateFinder
+from optuna.importance import get_param_importances
+from optuna.trial import TrialState
+from optuna_integration import PyTorchLightningPruningCallback
+from src.constants import DEFAULT_SEED
 from src.datamodule import LightningDataset
-from src.module import CEGANNModule
+from src.module import Module
 from src.transforms import LineGraph, RandomPerturbation
-from torch import optim
+
+EPOCHS = 50
 
 
 def objective(trial: optuna.trial.Trial) -> float:
-    n_conv_edge = trial.suggest_int("n_conv_edge", 2, 5)
-    edge_expansion_units = trial.suggest_categorical("edge_expansion_units", [32, 64, 128, 256])
-    angle_expansion_units = trial.suggest_categorical("angle_expansion_units", [32, 64, 128, 256])
+    k_neigh = trial.suggest_int("k_neigh", 6, 20)
+    edge_units = trial.suggest_categorical("edge_units", [32, 64, 128, 256, 512])
+    angle_units = trial.suggest_categorical("angle_units", [32, 64, 128, 256, 512])
     num_radial = trial.suggest_int("num_radial", 20, 100)
-    k_neigh = trial.suggest_int("k_neigh", 8, 24)
+    num_conv_edge = trial.suggest_int("num_conv_edge", 1, 6)
+    dropout = trial.suggest_float("dropout", 0.1, 0.8, step=0.1)
+    classifier_units = trial.suggest_categorical("classifier_units", [32, 64, 128, 256, 512])
+    classifier_layers = trial.suggest_int("classifier_layers", 1, 4)
 
-    # Data management
+    hyperparameters = {
+        "k_neigh": k_neigh,
+        "edge_expansion_units": edge_units,
+        "angle_expansion_units": angle_units,
+        "num_radial": num_radial,
+        "n_bond_conv": num_conv_edge,
+        "dropout": dropout,
+        "classification_units": classifier_units,
+        "classification_layers": classifier_layers,
+    }
+
+    # Check whether we already evaluated the sampled hyperparameters
+    # If it exists, then use the existing value as trial duplicated the parameters.
+    states_to_consider = (TrialState.COMPLETE, TrialState.PRUNED, TrialState.FAIL)
+    trials_to_consider = trial.study.get_trials(deepcopy=False, states=states_to_consider)
+    for t in reversed(trials_to_consider):
+        if trial.params == t.params:
+            return t.value if t.value is not None else 0.0
+
+    # Configure the Lightning Trainer
+    trainer = L.Trainer(
+        logger=True,
+        enable_checkpointing=False,
+        max_epochs=EPOCHS,
+        callbacks=[
+            BatchSizeFinder(steps_per_trial=10),
+            LearningRateFinder(min_lr=1e-5, max_lr=0.1),
+            PyTorchLightningPruningCallback(trial, monitor="val/acc"),
+        ],
+    )
+
+    # Configure the Lightning Module
     datamodule = LightningDataset(
-        dataset_name="custom",
-        lengths=(0.7, 0.2, 0.1),
-        batch_size=8,
+        dataset_name="csg",
+        lengths=(0.8, 0.2),
         use_imbalance_sampler=True,
         pre_transforms=LineGraph(),
         transforms=[
             T.NormalizeFeatures(["x", "edge_attr"]),
-            RandomPerturbation(),
+            RandomPerturbation(std=0.05),
         ],
         num_workers=5,
         k=k_neigh,
         rcut=6.0,
-        force_reload=False,
+        force_reload=True,
     )
 
-    model = CEGANNModule(
+    # Configure the Lightning Module
+    model_kwargs = hyperparameters.copy()
+    model_kwargs.pop("k_neigh", None)
+
+    model = Module(
         model_name="cegann",
-        model_kwargs={
-            "n_classes": datamodule.num_classes,
-            "n_conv_edge": n_conv_edge,
-            "edge_expansion_units": edge_expansion_units,
-            "angle_expansion_units": angle_expansion_units,
-            "rbf": {"num_radial": num_radial},
-            "sbf": {"num_radial": num_radial},
-        },
-        optimizer=optim.AdamW,
-        scheduler=optim.lr_scheduler.StepLR,
-        scheduler_params={"gamma": 0.5, "step_size": 100},
+        num_classes=datamodule.num_classes,
+        compile=True,
         metrics=torchmetrics.MetricCollection(
             {
                 "acc": torchmetrics.Accuracy(
@@ -53,14 +90,57 @@ def objective(trial: optuna.trial.Trial) -> float:
                 ),
             }
         ),
+        warmup=100,
+        max_iters=trainer.max_epochs * len(datamodule.train_dataloader()),  # type: ignore
+        model_kwargs=model_kwargs,
     )
 
-    trainer = pl.Trainer(check_val_every_n_epoch=5, max_epochs=10)
-    trainer.fit(model=model, datamodule=datamodule)
-    acc = trainer.test(datamodule=datamodule, ckpt_path="best")
+    # Log the hyperparameters and start the training
+    if trainer.logger is not None:
+        trainer.logger.log_hyperparams(hyperparameters)
+    trainer.fit(model, datamodule=datamodule)
 
-    return acc[0]["test/acc"]
+    return trainer.callback_metrics["val/acc"].item()
 
 
-study = optuna.create_study(direction="maximize")
-study.optimize(objective, n_trials=48)
+if __name__ == "__main__":
+    L.seed_everything(DEFAULT_SEED)
+
+    study_name = "cegann-00"
+    storage = f"sqlite:///{study_name}.db"
+
+    sampler_pkl_path = Path(f"{study_name}_sampler.pkl")
+    if sampler_pkl_path.exists():
+        with open(sampler_pkl_path, "rb") as f:
+            sampler = pickle.load(f)
+    else:
+        sampler = optuna.samplers.TPESampler()
+    pruner = optuna.pruners.HyperbandPruner()
+
+    study = optuna.create_study(
+        direction="maximize",
+        sampler=sampler,
+        pruner=pruner,
+        study_name="cegann-00",
+        storage=storage,
+        load_if_exists=True,
+    )
+    study.optimize(objective, n_trials=100)
+
+    with open(sampler_pkl_path, "wb") as f:
+        pickle.dump(study.sampler, f)
+
+    print(f"Number of finished trials: {len(study.trials)}")
+
+    best_trial = study.best_trial
+
+    print("Best trial:")
+    print(f"  Value: {best_trial.value}")
+    print("  Params: ")
+    hparams_importance = get_param_importances(study)
+    for (param, value), (_, importance) in zip(
+        best_trial.params.items(), hparams_importance.items()
+    ):
+        print(f"    {param}: {value} ({importance:.2f})")
+
+    study.trials_dataframe().to_csv("study.csv")
