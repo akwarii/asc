a569cefccb841495638e5cf280499ceade151cda
ghuynh <gael.huynh@gmail.com>
2025-02-10 19:23:24 +0100

    pre commit

--- scripts/compute_dataset.py
+++ scripts/compute_dataset.py
@@ -1,6 +1,7 @@
 from itertools import product
 
 from src.datasets import CSG, Aflow, CustomDataset, MaterialProject
+from torch_geometric.data import Dataset
 
 k = 12
 rcut = 6.0
@@ -10,7 +11,7 @@ datasets = [CustomDataset, Aflow, CSG, MaterialProject]
 knn = [10, 12, 14, 16]
 
 for dataset, k in product(datasets, knn):
-    data = dataset(force_reload=reload, k=k, rcut=rcut)
+    data: Dataset = dataset(force_reload=reload, k=k, rcut=rcut)
     print(dataset)
     print("Num classes: ", data.num_classes)
     data.print_summary()
