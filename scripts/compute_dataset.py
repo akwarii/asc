2138a7bc165aa4fcbee3e002316201c039181973
ghuynh <gael.huynh@gmail.com>
2025-02-11 10:02:59 +0100

    Add line graph computation

--- scripts/compute_dataset.py
+++ scripts/compute_dataset.py
@@ -1,23 +1,17 @@
 from src.datasets import CSG
-<<<<<<< HEAD
-=======
-from src.transforms.line_graph import LineGraph
->>>>>>> b9409e20c20ee718a93ab1d169351211166c6659
+from src.transforms import LineGraph
 
 rcut = 6.0
-reload = True
+reload = False
 
 for k in [10, 12, 14, 16]:
-<<<<<<< HEAD
-    dataset = CSG(force_reload=reload, k=k, rcut=rcut)
-=======
     dataset = CSG(
         pre_transform=LineGraph(),
         force_reload=reload,
         k=k,
         rcut=rcut,
     )
->>>>>>> b9409e20c20ee718a93ab1d169351211166c6659
     print(dataset)
     print("Num classes: ", dataset.num_classes)
     dataset.print_summary()
+    del dataset
