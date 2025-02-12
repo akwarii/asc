Fix incorrect shape in edge cases

src/models/cegann.py
@@ -145,18 +145,20 @@ class CEGANN(nn.Module):
         edge_attr = self.sbf(edge_attr)
 
         # Perform message passing
-        x, edge_attr = self._message_passing(x, edge_attr, edge_index)
+        x_m, edge_attr_m = self._message_passing(x, edge_attr, edge_index)
 
         # Expand edge features and angle features
-        x = self.linear_bond(x)
-        edge_attr = self.linear_angle(edge_attr)
+        x_l = self.linear_bond(x_m)
+        edge_attr_l = self.linear_angle(edge_attr_m)
 
         # Reshape bond features and angle features
         # This is useful as we want to sum over the k neighbors later
         # while PyG LineGraph implied stacking the features of the k neighbors.
-        k = edge_attr.size(1) + 1
-        x = x.reshape(x.size(0) // k, k, x.size(-1))
-        edge_attr = edge_attr.reshape(edge_attr.size(0) // k, k, k - 1, edge_attr.size(-1))
+        k = edge_attr.size(0) // x.size(0) + 1
+        num_atoms = x.size(0) // k
+
+        x = x_l.view(num_atoms, k, x_l.size(-1))
+        edge_attr = edge_attr_l.view(num_atoms, k, k - 1, edge_attr_l.size(-1))
 
         # Sum over edge features and angle features
         x = torch.sum(self.softplus(x), dim=1)

src/models/gat.py
@@ -15,8 +15,7 @@ class GATClassifier(GAT):  # noqa
         hidden_channels: int,
         num_layers: int,
         out_channels: int,
-        edge_dim: int | None = None,
-        num_radial: int | None = None,
+        num_radial: int,
         dropout: float = 0.0,
         act: str | Callable | None = "relu",
         act_kwargs: dict[str, Any] | None = None,
@@ -30,13 +29,9 @@ class GATClassifier(GAT):  # noqa
         classification_layers: int | None = 1,
         **kwargs,
     ) -> None:
-        if edge_dim is None and num_radial is None:
-            edge_dim = 1
-        if num_radial is not None:
-            edge_dim = num_radial
-
         v2 = kwargs.pop("v2", True)
         add_self_loops = kwargs.pop("add_self_loops", False)
+        edge_dim = num_radial
 
         super().__init__(
             in_channels=in_channels,
@@ -67,11 +62,8 @@ class GATClassifier(GAT):  # noqa
             plain_last=True,
         )
 
-        self.supports_expansion = False
-        if num_radial is not None:
-            self.supports_expansion = True
-            self.rbf = GaussianBasis(num_radial=num_radial)
-            self.sbf = GaussianBasis(num_radial=num_radial)
+        self.rbf = GaussianBasis(num_radial=num_radial)
+        self.sbf = GaussianBasis(num_radial=num_radial)
 
     def forward(  # noqa
         self,
@@ -89,14 +81,13 @@ class GATClassifier(GAT):  # noqa
         edge_index = data.edge_index
         edge_attr = data.edge_attr
 
-        if self.supports_expansion:
-            x = self.rbf(x)
-            edge_attr = self.sbf(edge_attr)
+        x_emb = self.rbf(x)
+        edge_attr_emb = self.sbf(edge_attr)
 
         emb = super().forward(
-            x=x,
+            x=x_emb,
             edge_index=edge_index,
-            edge_attr=edge_attr,
+            edge_attr=edge_attr_emb,
             batch=batch,
             batch_size=batch_size,
             num_sampled_nodes_per_hop=num_sampled_nodes_per_hop,
@@ -109,69 +100,3 @@ class GATClassifier(GAT):  # noqa
 
         out = torch.sum(emb.view(num_atoms, k, self.out_channels), dim=1)
         return self.mlp(out)
-
-
-if __name__ == "__main__":
-    import lightning as L
-    import torchmetrics
-
-    from src.datamodule import LightningDataset
-    from src.module import Module
-    from src.transforms import LineGraph, RandomPerturbation
-
-    L.seed_everything(42)
-
-    datamodule = LightningDataset(
-        dataset_name="custom",
-        lengths=(0.9, 0.1),
-        use_imbalance_sampler=True,
-        pre_transforms=LineGraph(),
-        transforms=[
-            RandomPerturbation(std=0.1),
-        ],
-        num_workers=2,
-        k=10,
-        rcut=6.0,
-        batch_size=1,
-        persistent_workers=True,
-    )
-    num_classes = datamodule.num_classes
-
-    # Configure the Lightning Trainer
-    trainer = L.Trainer(
-        fast_dev_run=True,
-        deterministic=True,
-        logger=False,
-        enable_checkpointing=False,
-    )
-
-    # Configure the Lightning Module
-    with trainer.init_module():
-        model = Module(
-            model_name="gat",
-            num_classes=num_classes,
-            compile=False,
-            metrics=torchmetrics.MetricCollection(
-                {
-                    "acc": torchmetrics.Accuracy(
-                        task="multiclass",
-                        num_classes=num_classes,
-                    ),
-                    "f1": torchmetrics.F1Score(
-                        task="multiclass",
-                        num_classes=num_classes,
-                    ),
-                }
-            ),
-            warmup=100,
-            max_iters=trainer.max_epochs * len(datamodule.train_dataloader()),  # type: ignore
-            model_kwargs={
-                "in_channels": -1,
-                "num_layers": 2,
-                "hidden_channels": 64,
-                "heads": 1,
-                # "edge_dim": 6,
-                "num_radial": 25,
-            },
-        )
-    trainer.fit(model, datamodule=datamodule)