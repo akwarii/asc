ea614cc79fed0c90bcf22734120e8c04bd7fef81
ghuynh <gael.huynh@gmail.com>
2026-01-28 16:57:40 +0100

    Try to use nano dataset

--- src/datasets/csg.py
+++ src/datasets/csg.py
@@ -69,7 +69,7 @@ class CSG(InMemoryDataset):
     @property
     def raw_file_names(self) -> list[str]:
         """Return the name of the downloaded files."""
-        return ["CSG_micro.csv"]
+        return ["CSG_nano.csv"]
 
     @property
     def processed_file_names(self) -> list[str]:
