945b41a4745675ca6299d95deab822f7a5dac0e1
ghuynh <gael.huynh@gmail.com>
2026-02-06 16:07:09 +0100

    fix type hint

--- src/models/layers/geo_conv.py
+++ src/models/layers/geo_conv.py
@@ -1,5 +1,5 @@
 from collections.abc import Callable
-from typing import Any
+from typing import Any, Optional
 
 import torch
 import torch.nn.functional as F
@@ -199,7 +199,7 @@ class GeometricConv(MessagePassing):
         res_edge_attr: OptTensor,
         index: Tensor,
         ptr: OptTensor,
-        dim_size: int | None,  # PyG doesn't handle python 3.10+ union types signatures yet
+        dim_size: Optional[int],  # noqa: UP007
     ) -> tuple[Tensor, Tensor]:
         """Edge update with GLU-like gating mechanism."""
         # Fused projection to compute the gate inputs
