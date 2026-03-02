3353a7d1ad8c6bd53da44efd2836c84a1246d1ac
ghuynh <gael.huynh@gmail.com>
2026-03-02 18:27:35 +0100

    add missing import

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,5 @@
 from .cli import KeyValueParserAction
+from .data import atomic_numbers, chemical_symbols
 from .dataset import random_split
 from .lattice import poscar_from_entry
 from .logger import RankedLogger
