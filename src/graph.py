03378bb763c56c44f5c85a43022018bf6616f6c4
ghuynh <gael.huynh@gmail.com>
2026-03-03 18:37:14 +0100

    Moved static methods out of PeriodicKNN and replace StringIO by temp files since OVITO can't handle them

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,6 @@
 import io
+import os
+import tempfile
 from collections.abc import Callable
 from functools import partial
 from pathlib import Path
@@ -13,7 +15,6 @@ import torch
 from ase import Atoms
 from freud.box import Box
 from freud.locality import AABBQuery
-from line_profiler import profile
 from ovito.data import DataCollection
 from ovito.io import import_file
 from torch import Tensor
@@ -47,6 +48,40 @@ def _get_graph_method(n_atoms: int) -> tuple[str, torch.device]:
         method = "torch"
 
     return method, device
+
+
+def detect_format_from_str(atoms_str: str) -> FileFormats:
+    """Detect the format of a structure given as a string.
+
+    Args:
+        atoms_str: A string containing the structure.
+
+    Returns:
+        The format of the structure.
+    """
+    text = atoms_str.strip()
+    first_line = text.splitlines()[0].strip()
+
+    if "_cell_length_a" in text and "_atom_site" in text:
+        return "cif"
+    elif first_line.startswith("POSCAR") or first_line.startswith("CONTCAR"):
+        return "vasp"
+    elif first_line.isdigit():
+        return "xyz"
+    elif first_line.startswith("ITEM:"):
+        return "lammps-dump-text"
+    elif "Masses" in text and "Atoms" in text:
+        return "lammps-data"
+    else:
+        lines = text.splitlines()
+        try:
+            float(lines[1].split()[0])  # scaling factor
+            if all(len(line.split()) == 3 for line in lines[2:5]):  # lattice
+                return "vasp"
+        except ValueError:
+            pass
+
+    raise ValueError("Cannot guess structure format from string content")
 
 
 class KNNGraph:
@@ -242,6 +277,59 @@ class KNNGraph:
         return data
 
 
+def type_id_to_atomic_number(type_id: Tensor, type_mapping: dict[int, int]) -> Tensor:
+    """Convert a tensor of type ids to atomic numbers using a mapping.
+
+    Args:
+        type_id: A tensor of shape (num_atoms,) containing the type ids of the atoms.
+        type_mapping: A dictionary mapping type ids to atomic numbers.
+
+    Returns:
+        A tensor of shape (num_atoms,) containing the atomic numbers of the atoms.
+    """
+    max_type_id = int(type_id.max().item())
+    mapping_tensor = torch.zeros(max_type_id + 1, dtype=torch.long)
+    for t_id, z in type_mapping.items():
+        mapping_tensor[t_id] = z
+
+    atomic_numbers = mapping_tensor[type_id]
+
+    return atomic_numbers
+
+
+def read_structure(representation: str | Path | DataCollection) -> DataCollection:
+    """Read a structure from a string, file path or OVITO DataCollection and return it in OVITO's
+    format.
+
+    Args:
+        representation: A string containing the structure, a file path to a structure file, or an
+            OVITO DataCollection object.
+    """
+    if isinstance(representation, DataCollection):
+        return representation
+
+    if isinstance(representation, Path):
+        if not representation.exists():
+            raise ValueError(f"The file {representation} does not exist.")
+        return import_file(representation).compute()
+
+    if isinstance(representation, str):
+        if os.path.isfile(representation):
+            return import_file(representation).compute()
+
+        # We have a string representation of the structure so we write it to a temporary file
+        # since OVITO's import_file function does not support file-like objects such as StringIO.
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp:
+            tmp.write(representation)
+            tmp_path = Path(tmp.name)
+
+        atoms = import_file(tmp_path).compute()
+        tmp_path.unlink(missing_ok=True)
+        return atoms
+
+    raise TypeError(f"Unsupported input type {type(representation)}")
+
+
 class PeriodicKNN:
     """Test of a periodic knn using Freud."""
 
@@ -251,22 +339,14 @@ class PeriodicKNN:
 
         self.k = k
 
-    @profile
     def _get_graph_data(self, atoms: DataCollection) -> tuple[Tensor, Tensor, Tensor]:
         # Map OVITO particle types to atomic numbers
         type_mapper = {t.id: atomic_numbers[t.name] for t in atoms.particles.particle_types.types}
+        type_id = torch.from_numpy(atoms.particles.particle_types[...]).long()
 
-        elements_id = torch.from_numpy(atoms.particles.particle_types[...]).long()
+        x = type_id_to_atomic_number(type_id, type_mapper)
 
-        max_type = int(elements_id.max().item())
-        mapping = torch.zeros(max_type + 1, dtype=torch.long)
-
-        for lmp_type, z in type_mapper.items():
-            mapping[lmp_type] = z
-
-        x = mapping[elements_id]
-
-        # extract data from ASE Atoms object
+        # Extract data from OVITO DataCollection object
         pos = atoms.particles.positions[...]
         cell_matrix = atoms.cell[...][:3, :3].T
 
@@ -291,34 +371,7 @@ class PeriodicKNN:
 
         return x, edge_index, edge_attr
 
-    @staticmethod
-    def _to_ovito(representation: str | Path | DataCollection | io.StringIO) -> DataCollection:
-        if isinstance(representation, DataCollection):
-            return representation
-
-        if isinstance(representation, io.StringIO):
-            representation.seek(0)
-
-        elif isinstance(representation, str):
-            if not Path(representation).exists():
-                representation = io.StringIO(representation.strip())
-                representation.seek(0)
-
-        elif isinstance(representation, Path):
-            if not representation.exists():
-                raise ValueError(f"The file {representation} does not exist.")
-
-        else:
-            raise TypeError(f"Unsupported input type {type(representation)}")
-
-        print(type(representation))
-        atoms = import_file(representation).compute()  # type: ignore
-
-        return atoms
-
-    def convert(
-        self, atoms_repr: str | Path | DataCollection | io.StringIO, fmt: str | None = None
-    ) -> Data:
+    def convert(self, atoms_repr: str | Path | DataCollection, fmt: str | None = None) -> Data:
         """Convert a single atomic structure to a PyG Data object.
 
         Args:
@@ -328,7 +381,7 @@ class PeriodicKNN:
         Returns:
             A PyG Data object with positions, edge index, distances and cosine of the angles.
         """
-        atoms = self._to_ovito(atoms_repr)
+        atoms = read_structure(atoms_repr)
 
         cell_obj = atoms.cell
         assert (
@@ -352,37 +405,3 @@ class PeriodicKNN:
         )
 
         return data
-
-
-def detect_format_from_str(atoms_str: str) -> FileFormats:
-    """Detect the format of a structure given as a string.
-
-    Args:
-        atoms_str: A string containing the structure.
-
-    Returns:
-        The format of the structure.
-    """
-    text = atoms_str.strip()
-    first_line = text.splitlines()[0].strip()
-
-    if "_cell_length_a" in text and "_atom_site" in text:
-        return "cif"
-    elif first_line.startswith("POSCAR") or first_line.startswith("CONTCAR"):
-        return "vasp"
-    elif first_line.isdigit():
-        return "xyz"
-    elif first_line.startswith("ITEM:"):
-        return "lammps-dump-text"
-    elif "Masses" in text and "Atoms" in text:
-        return "lammps-data"
-    else:
-        lines = text.splitlines()
-        try:
-            float(lines[1].split()[0])  # scaling factor
-            if all(len(line.split()) == 3 for line in lines[2:5]):  # lattice
-                return "vasp"
-        except ValueError:
-            pass
-
-    raise ValueError("Cannot guess structure format from string content")
