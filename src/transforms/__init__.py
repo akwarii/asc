afae893d9cde711f61a65fd1492222e0b484b141
ghuynh <gael.huynh@gmail.com>
2026-03-05 11:53:32 +0100

    run pre-commit

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -1,5 +1,5 @@
+from .box_scaling import BoxScaling
+from .box_shearing import BoxShearing
 from .dropout_node import DropoutNode
 from .line_graph import LineGraph
 from .random_perturbation import RandomPerturbation
-from .box_scaling import BoxScaling
-from .box_shearing import BoxShearing
