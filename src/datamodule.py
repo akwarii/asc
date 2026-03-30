891bc17733f00373c8c9a539718664a0dc615b47
ghuynh <gael.huynh@gmail.com>
2026-03-30 14:41:34 +0200

    Remove unecessary comment

--- src/datamodule.py
+++ src/datamodule.py
@@ -98,9 +98,6 @@ class LightningDataset(LightningDataModule):
         kwargs["num_workers"] = kwargs.get("num_workers", 0)
         kwargs["pin_memory"] = kwargs.get("pin_memory", True)
         kwargs["persistent_workers"] = kwargs.get("persistent_workers", kwargs["num_workers"] > 0)
-
-        # ?  We need to handle it explicitely for the CLI to accept it as a valid argument.
-        # ? `None` is arguably (?) the best default as it lets the runtime/platform decide.
         kwargs["multiprocessing_context"] = kwargs.get("multiprocessing_context", None)
 
         self.kwargs = kwargs
